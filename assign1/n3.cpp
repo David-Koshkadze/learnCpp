7803cbd5781b07e0e51ad5c71d46cc51e3e3d9fd
David-Koshkadze <rootUS3rr@pm.me>
2023-03-17 23:38:49 +0400

    ch12

---
+++ assign1/n3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float a, b, s;
+
+  cout << "a: ";
+  cin >> a;
+  cout << "b: ";
+  cin >> b;
+
+  s = a * b;
+
+  cout << "S = " << s << endl;
+  
+  return 0;
+}
