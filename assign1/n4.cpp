4d01cf19fa01141bcc092441d86740caf330c46a
David-Koshkadze <rootUS3rr@pm.me>
2023-03-17 23:51:25 +0400

    ch13

---
+++ assign1/n4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  double a = 75;
+
+  double ad = a / 7;
+  int a_floor = floor(a / 7);
+
+  cout << ad << endl;
+  cout << a_floor << endl;
+
+  return 0;
+}
